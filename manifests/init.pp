cdf49dfed2c2454458dabc152d70f3e106a9e2e0
MoOx <m@moox.fr>
2013-07-03 16:11:00 +0200

    Update package source url

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,7 +5,7 @@
 #   include ntfs_3g
 class ntfs_3g {
   package { 'NTFS_3G':
-    source   => 'http://sourceforge.net/projects/catacombae/files/NTFS-3G%20for%20Mac%20OS%20X/2010.10.2/ntfs-3g-2010.10.2-macosx.dmg/download',
+    source   => 'http://freefr.dl.sourceforge.net/project/catacombae/NTFS-3G%20for%20Mac%20OS%20X/2010.10.2/ntfs-3g-2010.10.2-macosx.dmg',
     provider => 'pkgdmg'
   }
 }
--- spec/classes/ntfs_3g_spec.rb
+++ spec/classes/ntfs_3g_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'ntfs_3g' do
   it do
     should contain_package('NTFS_3G').with({
-      :source   => 'http://sourceforge.net/projects/catacombae/files/NTFS-3G%20for%20Mac%20OS%20X/2010.10.2/ntfs-3g-2010.10.2-macosx.dmg/download',
+      :source   => 'http://freefr.dl.sourceforge.net/project/catacombae/NTFS-3G%20for%20Mac%20OS%20X/2010.10.2/ntfs-3g-2010.10.2-macosx.dmg',
       :provider => 'pkgdmg'
     })
   end
